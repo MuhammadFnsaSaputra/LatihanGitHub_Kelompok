c69fc0757f7cce52ba287586b115d24bae92c1ad
MuhammadFnsaSaputra <finsasa12@gmail.com>
2025-10-06 11:28:12 +0800

    update Form Login

--- LatihanGitHub_Kelompok/FormLogin.Designer.cs
+++ LatihanGitHub_Kelompok/FormLogin.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             menuStrip1 = new MenuStrip();
+            txtUsername = new TextBox();
+            lblUsername = new Label();
+            lblPassword = new Label();
+            txtPassword = new TextBox();
+            lblLogin = new Label();
+            btnLogin = new Button();
+            btnBack = new Button();
             SuspendLayout();
             // 
             // menuStrip1
@@ -36,15 +43,81 @@
             menuStrip1.ImageScalingSize = new Size(20, 20);
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.Size = new Size(800, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(130, 106);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(534, 27);
+            txtUsername.TabIndex = 1;
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(42, 109);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(82, 20);
+            lblUsername.TabIndex = 2;
+            lblUsername.Text = "Username :";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(47, 190);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(77, 20);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Password :";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(130, 187);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(534, 27);
+            txtPassword.TabIndex = 4;
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Location = new Point(341, 34);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(79, 20);
+            lblLogin.TabIndex = 5;
+            lblLogin.Text = "Login User";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(130, 335);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 6;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(570, 335);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(94, 29);
+            btnBack.TabIndex = 7;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
             // 
             // FormLogin
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnBack);
+            Controls.Add(btnLogin);
+            Controls.Add(lblLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(txtUsername);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "FormLogin";
@@ -56,5 +129,12 @@
         #endregion
 
         private MenuStrip menuStrip1;
+        private TextBox txtUsername;
+        private Label lblUsername;
+        private Label lblPassword;
+        private TextBox txtPassword;
+        private Label lblLogin;
+        private Button btnLogin;
+        private Button btnBack;
     }
 }
