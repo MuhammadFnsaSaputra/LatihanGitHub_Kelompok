9e9d22832c51f04e93ab6d74501dabf0108918a8
MuhammadFnsaSaputra <finsasa12@gmail.com>
2025-10-13 09:36:26 +0800

    formlogin new

--- LatihanGitHub_Kelompok/FormJurusan.Designer.cs
+++ LatihanGitHub_Kelompok/FormJurusan.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormJurusan";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            lblid = new Label();
+            lblnisn = new Label();
+            lbljurusan = new Label();
+            dataGridView1 = new DataGridView();
+            btnsimpan = new Button();
+            btnhapus = new Button();
+            btnkembali = new Button();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(153, 52);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(182, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(153, 110);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(182, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(153, 165);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(182, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // lblid
+            // 
+            lblid.AutoSize = true;
+            lblid.Location = new Point(103, 59);
+            lblid.Name = "lblid";
+            lblid.Size = new Size(29, 20);
+            lblid.TabIndex = 3;
+            lblid.Text = "Id :";
+            // 
+            // lblnisn
+            // 
+            lblnisn.AutoSize = true;
+            lblnisn.Location = new Point(82, 117);
+            lblnisn.Name = "lblnisn";
+            lblnisn.Size = new Size(50, 20);
+            lblnisn.TabIndex = 4;
+            lblnisn.Text = "NISN :";
+            // 
+            // lbljurusan
+            // 
+            lbljurusan.AutoSize = true;
+            lbljurusan.Location = new Point(68, 172);
+            lbljurusan.Name = "lbljurusan";
+            lbljurusan.Size = new Size(64, 20);
+            lbljurusan.TabIndex = 5;
+            lbljurusan.Text = "Jurusan :";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dataGridView1.Location = new Point(68, 344);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(427, 188);
+            dataGridView1.TabIndex = 6;
+            // 
+            // btnsimpan
+            // 
+            btnsimpan.Location = new Point(68, 273);
+            btnsimpan.Name = "btnsimpan";
+            btnsimpan.Size = new Size(94, 29);
+            btnsimpan.TabIndex = 7;
+            btnsimpan.Text = "Simpan";
+            btnsimpan.UseVisualStyleBackColor = true;
+            // 
+            // btnhapus
+            // 
+            btnhapus.Location = new Point(241, 273);
+            btnhapus.Name = "btnhapus";
+            btnhapus.Size = new Size(94, 29);
+            btnhapus.TabIndex = 8;
+            btnhapus.Text = "Hapus";
+            btnhapus.UseVisualStyleBackColor = true;
+            // 
+            // btnkembali
+            // 
+            btnkembali.Location = new Point(401, 273);
+            btnkembali.Name = "btnkembali";
+            btnkembali.Size = new Size(94, 29);
+            btnkembali.TabIndex = 9;
+            btnkembali.Text = "Kembali";
+            btnkembali.UseVisualStyleBackColor = true;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Id";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "NISN";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Jurusan";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.Width = 125;
+            // 
+            // FormJurusan
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1052, 564);
+            Controls.Add(btnkembali);
+            Controls.Add(btnhapus);
+            Controls.Add(btnsimpan);
+            Controls.Add(dataGridView1);
+            Controls.Add(lbljurusan);
+            Controls.Add(lblnisn);
+            Controls.Add(lblid);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "FormJurusan";
+            Text = "FormJurusan";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label lblid;
+        private Label lblnisn;
+        private Label lbljurusan;
+        private DataGridView dataGridView1;
+        private Button btnsimpan;
+        private Button btnhapus;
+        private Button btnkembali;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
     }
 }
